e5179ae379e56b090ada2151a9935a9640f7f7c6
dhamolahedonist <dhamolahedonist>
2023-09-07 16:07:02 +0100

    adjusted utc time

--- app.js
+++ app.js
@@ -10,39 +10,3 @@ app.use("/api", appRoute);
 app.listen(PORT, () => {
   console.log(`App is listening at port ${PORT}`);
 });
-
-// const express = require("express");
-// const helper = require("./helper");
-
-// require("dotenv").config();
-// const PORT = process.env.PORT;
-
-// const app = express();
-
-// app.use(express.json());
-
-// const currentDayName = helper.currentDayName();
-
-// const currentUtcTime = helper.getCurrentUtcTime();
-
-// app.get("/api", (req, res) => {
-//   const { slack_name, track } = req.query;
-//   const responseJson = {
-//     slack_name: slack_name,
-//     current_day: currentDayName,
-//     utc_time: currentUtcTime,
-//     track: track,
-//     github_file_url:
-//       "https://github.com/dhamolahedonist/zuriTaskOne/blob/main/README.md",
-//     github_repo_url: "https://github.com/dhamolahedonist/zuriTaskOne",
-//     status_code: 200,
-//   };
-//   res.setHeader("Content-Type", "application/json"); // Set the Content-Type header
-//   res.status(200).json(responseJson); // Send the JSON response
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server started on PORT: http://localhost:${PORT}`);
-// });
-
-// module.exports = app;
