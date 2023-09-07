dda010b7fc7cf6e9d64a474ec5585f4241e6eb20
dhamolahedonist <dhamolahedonist>
2023-09-07 13:08:14 +0100

    adjusted utc time

--- helper.js
+++ helper.js
@@ -16,15 +16,26 @@ const helper = {
     return currentDayName;
   },
   getCurrentUtcTime: () => {
-    const now = new Date();
-    const year = now.getUTCFullYear();
-    const month = String(now.getUTCMonth() + 1).padStart(2, "0");
-    const day = String(now.getUTCDate()).padStart(2, "0");
-    const hours = String(now.getUTCHours()).padStart(2, "0");
-    const minutes = String(now.getUTCMinutes()).padStart(2, "0");
-    const seconds = String(now.getUTCSeconds()).padStart(2, "0");
-    const currentTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
-    return currentTime;
+    const currentDate = new Date();
+    const formattedUtcTime = `${currentDate.getUTCFullYear()}-${(
+      currentDate.getUTCMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}-${currentDate
+      .getUTCDate()
+      .toString()
+      .padStart(2, "0")}T${currentDate
+      .getUTCHours()
+      .toString()
+      .padStart(2, "0")}:${currentDate
+      .getUTCMinutes()
+      .toString()
+      .padStart(2, "0")}:${currentDate
+      .getUTCSeconds()
+      .toString()
+      .padStart(2, "0")}Z`;
+
+    return formattedUtcTime;
   },
 };
 module.exports = helper;
